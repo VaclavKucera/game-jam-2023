c5c25384374a1f27ad2673b5f8946a1da9c36177
VaclavKucera <kucera.vcv@gmail.com>
2023-05-05 23:55:07 +0200

    movement

--- game-jam-2023/Assets/Scripts/Movement.cs
+++ game-jam-2023/Assets/Scripts/Movement.cs
@@ -1,12 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float dashSpeed = 15.0f;
+    public float dashDuration = 0.3f;
+    public float dashRecoveryDuration = 1f;
+
+    private float moveInputHorizontal;
+    private float moveInputVertical;
     private Rigidbody2D rb;
+    private bool dashing;
+    private bool canDash = true;
 
     void Start()
     {
@@ -15,13 +21,39 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        var moveInputHorizontal = Input.GetAxis("Horizontal");
-        var moveInputVertical = Input.GetAxis("Vertical");
+        moveInputHorizontal = Input.GetAxis("Horizontal");
+        moveInputVertical = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.Space)) Debug.Log("Space");
+        if (Input.GetKey(KeyCode.Space) && canDash)
+        {
+            Debug.Log("Dash");
+            dashing = true;
+            canDash = false;
+            StartCoroutine(Dash());
+        }
 
-        var speed = Input.GetKey(KeyCode.Space) ? dashSpeed : moveSpeed;
+        var speed = dashing ? dashSpeed : moveSpeed;
 
         rb.velocity = new Vector2(moveInputHorizontal * speed, moveInputVertical * speed);
+    }
+
+    IEnumerator Dash()
+    {
+        var dashTime = dashDuration;
+        while (dashTime > 0)
+        {
+            dashTime -= Time.deltaTime;
+            yield return null;
+        }
+        dashing = false;
+
+        var dashRecoveryTime = dashRecoveryDuration;
+        while (dashRecoveryTime > 0)
+        {
+            dashRecoveryTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        canDash = true;
     }
 }
