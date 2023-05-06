8995e7b23052aaffdddd9978f7a754b5821c428b
VaclavKucera <kucera.vcv@gmail.com>
2023-05-06 18:02:00 +0200

    totem generation

--- game-jam-2023/Assets/Scripts/Library/RandomGeneration.cs
+++ game-jam-2023/Assets/Scripts/Library/RandomGeneration.cs
@@ -5,16 +5,16 @@ namespace Library
     public static class RandomGeneration
     {
         private static readonly Vector3 DefaultCenter = Vector3.zero;
-        private const float DefaultRadius = 5;
+        private const float DefaultRadius = 3;
 
         public static Vector3 RandomPosition(Vector3? center = null, float? radius = null)
         {
             var r = radius ?? DefaultRadius;
             var c = center ?? DefaultCenter;
             return new Vector3(
-                Random.Range(c.x, c.x + r),
-                Random.Range(c.y, c.y + r),
-                Random.Range(c.z, c.z + r)
+                Random.Range(c.x - r, c.x + r),
+                Random.Range(c.y - r, c.y + r),
+                Random.Range(c.z - r, c.z + r)
             );
         }
     }
